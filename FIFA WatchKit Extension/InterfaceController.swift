ae0c2f9c67b35275f650c9fb5d3bf3ad49800f9b
Gandhi89 <shivamgandhi95@gmail.com>
2019-03-07 11:47:38 -0500

    Initial Commit

---
+++ FIFA WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  FIFA WatchKit Extension
+//
+//  Created by shivam gandhi on 2019-03-07.
+//  Copyright © 2019 shivam gandhi. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
